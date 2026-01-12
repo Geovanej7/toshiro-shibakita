be762379315d80b08cfe205ece5ec730bb7e5edf
Geovane Júnior <122648367+Geovanej7@users.noreply.github.com>
2026-01-12 16:05:45 -0300

    Update banco.sql

--- banco.sql
+++ banco.sql
@@ -1,8 +1,6 @@
 CREATE TABLE dados (
-    AlunoID int,
-    Nome varchar(50),
-    Sobrenome varchar(50),
-    Endereco varchar(150),
-    Cidade varchar(50),
-    Host varchar(50)
+    ProdutoID int,
+    Titulo varchar(50),
+    descricao varchar(250),
+    preco double
 );
